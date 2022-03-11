2d73b9439fd953a1720c145c6677ce8be1b7c236
João Inácio <jgci2000@hotmail.com>
2022-03-11 17:19:07 +0100

    bootstrap working

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from ensurepip import bootstrap
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,9 +11,8 @@ Nb = N
 
 # MC Cycles
 therm_cycles = int(1e3)
-mc_cycles = int(1e3)
-bootstrap_cycles = 100
-n_sim = 20
+mc_cycles = int(1e4)
+bootstrap_cycles = mc_cycles
 
 beta_vals = np.power(2.0, [-1, 0, 1, 2, 3, 4])
 mean_n = np.zeros(beta_vals.shape)
@@ -22,31 +20,26 @@ std_n = np.zeros(beta_vals.shape)
 mean_E = np.zeros(beta_vals.shape)
 std_E = np.zeros(beta_vals.shape)
 
+n_vals = np.zeros((len(beta_vals), mc_cycles))
 for i, beta in enumerate(beta_vals):
-    mean_E_tmp = np.zeros(n_sim)
-    mean_n_tmp = np.zeros(n_sim)
-    n_vals_tmp = np.zeros((n_sim, mc_cycles))
     
-    for j in range(n_sim):
-        print(f"beta = {i+1}/{len(beta_vals)} and simulation {j+1:2d}/{n_sim}", end='\r')
-        
-        rng = np.random.default_rng(np.random.MT19937())
-        n_vals_tmp[j, :] = sse(beta, N, Nb, therm_cycles, mc_cycles, rng)
-
-        mean_n_tmp[j] = np.mean(n_vals_tmp[j, :])
+    print(f"beta = {i+1}/{len(beta_vals)}", end='\r')
     
-    mean_n_bs = np.zeros((bootstrap_cycles, n_sim))
-    mean_E_bs = np.zeros((bootstrap_cycles, n_sim))
+    rng = np.random.default_rng(np.random.MT19937())
+    n_vals[i, :] = sse(beta, N, Nb, therm_cycles, mc_cycles, rng)
+    
+    mean_n_bs = np.zeros((bootstrap_cycles, mc_cycles))
+    mean_E_bs = np.zeros((bootstrap_cycles, mc_cycles))
     
     for bs_cycle in range(bootstrap_cycles):
-        mean_n_bs[bs_cycle, :] = resample(mean_n_tmp)
+        mean_n_bs[bs_cycle, :] = resample(n_vals[i, :])
         mean_E_bs[bs_cycle, :] = - mean_n_bs[bs_cycle, :] / (beta * N)
     
     mean_E[i] = np.mean(mean_E_bs)
-    std_E[i] = np.mean(np.std(mean_E_bs, axis=0))
+    std_E[i] = np.mean(np.std(mean_E_bs, axis=1))
     
     mean_n[i] = np.mean(mean_n_bs)
-    std_n[i] = np.mean(np.std(mean_n_bs, axis=0))
+    std_n[i] = np.mean(np.std(mean_n_bs, axis=1))
 
 for i, beta in enumerate(beta_vals):
     print(f"beta : {beta} | <E> = {mean_E[i]:.6f} | std_E = {std_E[i]:.6f} | <n> = {mean_n[i]:.2f} | std_n = {std_n[i]:.2f}")
