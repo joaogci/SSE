5c752cc3189a57a3e911cb70f924de4e3502dbdd
João Inácio <jgci2000@hotmail.com>
2022-04-19 10:21:41 +0200

    optimizations and new thermodynamic variables

--- src/test.c
+++ src/test.c
@@ -15,7 +15,7 @@ double mean(int *arr, int size) {
 
 
 int main(int argc, char **argv) {
-    int N = 2;
+    int N = 16;
     double beta;
     double beta_vals[6] = {0.5, 1.0, 2.0, 4.0, 8.0, 16.0};
 
@@ -27,6 +27,7 @@ int main(int argc, char **argv) {
 
     int t;
     int n_vals[mc_cycles];
+    int n2_vals[mc_cycles];
 
     for (int i = 0; i < 6; i++) {
         beta = beta_vals[i];
@@ -39,16 +40,22 @@ int main(int argc, char **argv) {
             loop_update(sse_state, vertex);
             ajust_cutoff(sse_state, vertex);
         }
-        
+
         for (t = 0; t < mc_cycles; t++) {
             diag_update(sse_state, vertex);
             create_vertex_list(sse_state, vertex);
             loop_update(sse_state, vertex);
 
             n_vals[t] = sse_state->n;
+            n2_vals[t] = sse_state->n * sse_state->n;
         }
 
-        printf("beta: %f | E_mean: %f | n_mean %f \n", beta, - mean(n_vals, mc_cycles) / (N * beta) + 0.25, mean(n_vals, mc_cycles));
+        double n_mean = mean(n_vals, mc_cycles);
+        double n2_mean = mean(n2_vals, mc_cycles);
+        double E_mean = - n_mean / (N * beta) + 0.25;
+        double C_mean = (n2_mean - n_mean * (n_mean - 1)) / N;
+
+        printf("beta: %f | E_mean: %f | C_mean: %f | n_mean %f \n", beta, E_mean, C_mean, n_mean);
     }
 
     free_state(&sse_state);
