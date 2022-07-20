1f3ae5acdf7fd570018ee2086640f6edf6a7195e
Joao Inacio <jgci2000@hotmail.com>
2022-07-20 20:59:27 +0100

    removed comments

--- src/rng/xorshiro256++.h
+++ src/rng/xorshiro256++.h
@@ -29,27 +29,6 @@ static inline uint64_t rotl(const uint64_t x, int k) {
 
 static uint64_t s[4];
 
-// uint64_t next(uint64_t *s) {
-// 	const uint64_t result = rotl(s[0] + s[3], 23) + s[0];
-
-// 	const uint64_t t = s[1] << 17;
-
-// 	s[2] ^= s[0];
-// 	s[3] ^= s[1];
-// 	s[1] ^= s[2];
-// 	s[0] ^= s[3];
-
-// 	s[2] ^= t;
-
-// 	s[3] = rotl(s[3], 45);
-
-// 	return result;
-// }
-
-// double next_double(uint64_t *s) {
-//     return (next(s) >> 11) * 0x1.0p-53;
-// }
-
 static inline uint64_t next(void) {
 	const uint64_t result = rotl(s[0] + s[3], 23) + s[0];
 
