f104ea1ad49102d38a6b41a64d43d61178dca83e
João Inácio <j.g.c.inacio@fys.uio.no>
2022-09-19 12:32:51 +0200

    better output information

--- run.sh
+++ run.sh
@@ -7,13 +7,12 @@ usage() {
 Usage               : $PROGNAME [-b] [-i <input_name>] [-h] [-n <n_threads>] [-o <output_name>] [-s <series_name>] [-t]
 
 -b                  : Use Heat Bath for transition probabilities (slower method)
--c                  : Clear vtx tmp directory.
 -i <input_name>     : Relative path + name of input file for simulation. Default value: "input.txt"
 -h                  : Help. Shows this text.
 -n <n_threads>      : Set number of threads for openMP. Default value: "4"
 -o <output_name>    : Relative path + name of output file for simulation. csv file is more convinient. Default value: "output.csv"
 -s <series_name>    : Relative path + name of correlation series file for simluation. csv is more convinient.
--t                  : Test mode.
+-t                  : Test mode. Uses a pre-fixed seed (2) and runs with the test temperatures (1/T = {0.5, 1.0, 2.0, 4.0, 8.0, 16.0}).
 
 EOF
     exit 1
@@ -24,57 +23,48 @@ output_name="output.csv"
 series_name=""
 n_threads="4"
 test=""
-clear=0
 hb=0
 
-while getopts bci:hn:o:s:t opts; do
+while getopts bi:hn:o:s:t opts; do
     case $opts in 
         (b) hb=1;;
-        (c) clear=1;;
         (i) input_name=$OPTARG;;
         (h) usage;;
         (n) n_threads=$OPTARG;;
         (o) output_name=$OPTARG;;
-        (s) series_name=$OPTARG; series="series";;
+        (s) series_name=$OPTARG; series="series"; n_threads="1";;
         (t) test="test";;
         (:) echo "Option -$OPTARG requires an argument." >&2 ; exit 1;;
         (*) usage
     esac
 done
 
-if [ $clear -eq 1 ]; then 
-    echo " ------ "
-    echo "Cleaning tmp directory."
-    rm -r src/vtx/tmp/*
-fi
-
-echo " ------ "
-echo "Checking if tmp directory exists."
-if [ -d "src/vtx/tmp" ]; then
+echo "[1] - Checking if tmp directory exists."
+if [ -d "tmp" ]; then
     echo "tmp directoty exists."
 else 
     echo "tmp directory does not exist. Creating directory."
-    mkdir src/vtx/tmp
+    mkdir tmp
 fi
-echo " ------ "
 
-echo "Generating vertex information."
+echo "[2] - Generating vertex information."
 vtx_name=$(python3 src/vtx/gen_vtx.py $input_name $hb)
 echo "Saved to file $vtx_name"
-echo " ------ "
 
-echo "Compiling program."
+echo "[3] - Compiling program."
 cd src
 if [[ "$series" == "series" ]]; then
     make $series
 else
     make $test
 fi
-echo " ------ "
 
-echo "Running the simulation."
+cd ..
+echo "[4] - Running the simulation."
 echo
-./main $n_threads ../$input_name vtx/tmp/$vtx_name ../$output_name ../$series_name 
+./src/main $n_threads $input_name tmp/$vtx_name $output_name $series_name 
+echo 
 
-
+echo "[5] - Removing vertex file."
+rm tmp/${vtx_name}
 
