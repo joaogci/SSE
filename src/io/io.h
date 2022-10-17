be09cf5c3af0a33941221db2ffad7a6bcfba618d
João Inácio <j.g.c.inacio@fys.uio.no>
2022-10-17 10:21:07 +0200

    removed autocorrelation time series

--- src/io/io.h
+++ src/io/io.h
@@ -12,9 +12,4 @@ void read_inputs(char *file_name, int *d, int *L, double *J, double *delta, doub
 void read_vtx_info(char *file_name, vtx_element **vtx, int *n_diagrams);
 void write_outputs(char *file_name, sampled_quantities *samples);
 
-#ifdef M_AUTOCORRELATION
-    # include "../sampling/autocorrelation.h"
-    void write_autocorrelation(char *file_name, autocorrelation *corr_series);
-#endif
-
 #endif // IO_H
