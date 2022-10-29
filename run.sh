ae27fbab97867bd6d16e1d713d8e9d289aaa8630
João Inácio <j.g.c.inacio@fys.uio.no>
2022-10-29 19:12:22 +0200

    removed series and heatbath

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ usage() {
 
 Usage               : $PROGNAME [-b] [-i <input_name>] [-h] [-n <n_threads>] [-o <output_name>] [-s <series_name>] [-t]
 
--b                  : Use Heat Bath for transition probabilities (slower method)
 -i <input_name>     : Relative path + name of input file for simulation. Default value: "input.txt"
 -h                  : Help. Shows this text.
 -n <n_threads>      : Set number of threads for openMP. Default value: "4"
@@ -21,11 +20,9 @@ input_name="input.txt"
 output_name="output.csv"
 n_threads="4"
 test=""
-hb=0
 
 while getopts bi:hn:o:t opts; do
     case $opts in 
-        (b) hb=1;;
         (i) input_name=$OPTARG;;
         (h) usage;;
         (n) n_threads=$OPTARG;;
@@ -45,7 +42,7 @@ else
 fi
 
 echo "[2] - Generating vertex information."
-vtx_name=$(python3 src/vtx/gen_vtx.py $input_name $hb)
+vtx_name=$(python3 src/vtx/gen_vtx.py $input_name)
 echo "Saved to file $vtx_name"
 
 echo "[3] - Compiling program."
