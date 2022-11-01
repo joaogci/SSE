594508833638e9ffdf362e138fb18d6c069a728a
João Inácio <j.g.c.inacio@fys.uio.no>
2022-11-01 13:48:25 +0100

    dm_s works now for general spin

--- src/sampling/sampling.c
+++ src/sampling/sampling.c
@@ -41,6 +41,7 @@ void sample(int n, int t_idx, heisenberg_system *system, sse_state *state, sampl
         if (state->op_string[p] % 3 != 0) {
             int b = (state->op_string[p] / 3) - 1;
             int a = state->op_string[p] % 3;
+            int old_state = system->spin[system->bond[b][0]];
 
             if (a == 1) {
                 system->spin[system->bond[b][0]] += 2;
@@ -50,8 +51,8 @@ void sample(int n, int t_idx, heisenberg_system *system, sse_state *state, sampl
                 system->spin[system->bond[b][1]] += 2;
             }
 
-            ms += 2.0 * pow(- 1.0, system->bond[b][0]) 
-            * system->spin[system->bond[b][0]];
+            ms += pow(- 1.0, system->bond[b][0]) 
+            * (system->spin[system->bond[b][0]] - old_state);
         }
 
         if (state->op_string[p] != 0) {
