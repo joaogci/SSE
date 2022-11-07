2b337177442c53b81ed260de1fa38d39f57347f0
João Inácio <j.g.c.inacio@fys.uio.no>
2022-11-07 10:25:26 +0100

    started works for post processing for sse simulation

--- plot.py
+++ plot.py
@@ -1,54 +1,15 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from post_processing import read_sse_output
 
 plt.style.use('seaborn')
 matplotlib.rcParams['mathtext.fontset'] = 'stix'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
-# Test temperatures
-# T_vals = 6
-# beta_vals = np.array([0.5, 1.0, 2.0, 4.0, 8.0, 16.0])
-# T = 1.0 / beta_sim
-
-# Custom temperatures
-Ti = 0.05
-Tf = 4.0
-T_vals = 50
-T = np.zeros(T_vals)
-for i in range(T_vals):
-    T[i] = (Tf - Ti) * (i + 1) / (T_vals)
-
-beta = 1.0 / T
-
 # Read the output file
-FILENAME = "2D_L4_FM_Heisenberg_delta1.0_h0.0.csv" # "2D_L8_FM_Heisenberg_delta1.0_h0.0.csv"
-
-E = np.zeros(T_vals)
-E_std = np.zeros(T_vals)
-C = np.zeros(T_vals)
-C_std = np.zeros(T_vals)
-m = np.zeros(T_vals)
-m_std = np.zeros(T_vals)
-m2 = np.zeros(T_vals)
-m2_std = np.zeros(T_vals)
-m_sus = np.zeros(T_vals)
-m_sus_std = np.zeros(T_vals)
-ms = np.zeros(T_vals)
-ms_std = np.zeros(T_vals)
-m2s = np.zeros(T_vals)
-m2s_std = np.zeros(T_vals)
-binder = np.zeros(T_vals)
-binder_std = np.zeros(T_vals)
-
-with open(FILENAME, "r") as f:
-    header = f.readline()
-    for j in range(T_vals):
-        _, _, _, _, E[j], E_std[j], C[j], \
-        C_std[j], m[j], m_std[j], m2[j], \
-        m2_std[j], _, _, ms[j], ms[j], \
-        m2s[j], m2s_std[j], _, _, m_sus[j], \
-        m_sus_std[j], binder[j], binder_std[j] = [float(x) for x in f.readline().strip().split(",")]
+FILENAME = "output.csv"
+sim_info, sampled = read_sse_output(FILENAME)
 
 # Plot the variables
 fig, _ = plt.subplots(3, 3, sharex=True)
@@ -56,49 +17,43 @@ plt.figure(1)
 fig.suptitle(FILENAME)
 
 plt.subplot(3, 3, 1)
-plt.errorbar(T, E, E_std, fmt=".--", label="SSE")
+plt.errorbar(sampled["T"], sampled["E"], sampled["E_std"], fmt=".--")
 plt.xlabel(r"$T$")
 plt.ylabel(r"$\langle E \rangle$")
 plt.legend()
 
 plt.subplot(3, 3, 2)
-plt.errorbar(T, C, C_std, fmt=".--", label="SSE")
+plt.errorbar(sampled["T"], sampled["C"], sampled["C_std"], fmt=".--")
 plt.xlabel(r"$T$")
 plt.ylabel(r"$C$")
 plt.legend()
 
-plt.subplot(3, 3, 3)
-plt.errorbar(T, binder, binder_std, fmt=".--", label="SSE")
-plt.xlabel(r"$T$")
-plt.ylabel(r"Binder Parameter")
-plt.legend()
-
 plt.subplot(3, 3, 4)
-plt.errorbar(T, m2, ms_std, fmt=".--", label="SSE")
+plt.errorbar(sampled["T"], sampled["m2"], sampled["m2_std"], fmt=".--")
 plt.xlabel(r"$T$")
 plt.ylabel(r"$\langle m_s \rangle$")
 plt.legend()
 
 plt.subplot(3, 3, 5)
-plt.errorbar(T, m2s, m2s_std, fmt=".--", label="SSE")
+plt.errorbar(sampled["T"], sampled["m2s"], sampled["m2s_std"], fmt=".--")
 plt.xlabel(r"$T$")
 plt.ylabel(r"$\langle m_s^2 \rangle$")
 plt.legend()
 
 plt.subplot(3, 3, 7)
-plt.errorbar(T, m, m_std, fmt=".--", label="SSE")
+plt.errorbar(sampled["T"], sampled["m"], sampled["m_std"], fmt=".--")
 plt.xlabel(r"$T$")
 plt.ylabel(r"$\langle m \rangle$")
 plt.legend()
 
 plt.subplot(3, 3, 8)
-plt.errorbar(T, m2, m2_std, fmt=".--", label="SSE")
+plt.errorbar(sampled["T"], sampled["m2"], sampled["m2_std"], fmt=".--")
 plt.xlabel(r"$T$")
 plt.ylabel(r"$\langle m^2 \rangle$")
 plt.legend()
 
 plt.subplot(3, 3, 9)
-plt.errorbar(T, m_sus, m_sus_std, fmt=".--", label="SSE")
+plt.errorbar(sampled["T"], sampled["m_sus"], sampled["m_sus_std"], fmt=".--")
 plt.xlabel(r"$T$")
 plt.ylabel(r"$\chi$")
 plt.legend()
---
+++ post_processing.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+def read_sse_output(filename):
+    """
+        reads SSE simulation outputs
+        
+        parameters:
+            filename - path for the output file
+            
+        output:
+            dictionary with all of the sampled quantitites
+            dictionary with simulation info
+    """
+    sim_info = dict()
+    sampled = dict()
+    
+    with open(filename, "r") as file:
+        # read simulation info
+        file.readline() 
+        line = file.readline().strip().split(',')
+        print(line)
+        sim_info["d"] = int(line[0])
+        sim_info["L"] = int(line[1])
+        sim_info["S"] = float(line[2])
+        sim_info["delta"] = float(line[3])
+        sim_info["h"] = float(line[4])
+        sim_info["epsilon"] = float(line[5])
+        
+        file.readline()
+        line = file.readline().strip().split(',')
+        sim_info["therm_cycles"]= int(line[0])
+        sim_info["mc_cycles"] = int(line[1])
+        sim_info["n_bins"] = int(line[2])
+        
+        file.readline()
+        line = file.readline().strip().split(',')
+        sim_info["cpu_time"] = float(line[0])
+        sim_info["n_threads"] = int(line[1])
+        
+        file.readline()
+        line = file.readline().strip().split(',')
+        sim_info["n_betas"] = int(line[0])
+        
+        # read sampled quantities
+        sampled["beta"] = np.zeros(sim_info["n_betas"])
+        sampled["T"] = np.zeros(sim_info["n_betas"])
+        sampled["n"] = np.zeros(sim_info["n_betas"])
+        sampled["n_std"] = np.zeros(sim_info["n_betas"])
+        sampled["E"] = np.zeros(sim_info["n_betas"])
+        sampled["E_std"] = np.zeros(sim_info["n_betas"])
+        sampled["C"] = np.zeros(sim_info["n_betas"])
+        sampled["C_std"] = np.zeros(sim_info["n_betas"])
+        sampled["m"] = np.zeros(sim_info["n_betas"])
+        sampled["m_std"] = np.zeros(sim_info["n_betas"])
+        sampled["m2"] = np.zeros(sim_info["n_betas"])
+        sampled["m2_std"] = np.zeros(sim_info["n_betas"])
+        sampled["m4"] = np.zeros(sim_info["n_betas"])
+        sampled["m4_std"] = np.zeros(sim_info["n_betas"])
+        sampled["ms"] = np.zeros(sim_info["n_betas"])
+        sampled["ms_std"] = np.zeros(sim_info["n_betas"])
+        sampled["m2s"] = np.zeros(sim_info["n_betas"])
+        sampled["m2s_std"] = np.zeros(sim_info["n_betas"])
+        sampled["m4s"] = np.zeros(sim_info["n_betas"])
+        sampled["m4s_std"] = np.zeros(sim_info["n_betas"])
+        sampled["m_sus"] = np.zeros(sim_info["n_betas"])
+        sampled["m_sus_std"] = np.zeros(sim_info["n_betas"])
+
+        file.readline()
+        for j in range(sim_info["n_betas"]):
+            sampled["beta"][j], sampled["n"][j], _, sampled["n_std"][j], \
+            sampled["E"][j], sampled["E_std"][j], sampled["C"][j], sampled["C_std"][j], \
+            sampled["m"][j], sampled["m_std"][j], sampled["m2"][j], sampled["m2_std"][j], \
+            sampled["m4"][j], sampled["m4_std"][j], sampled["ms"][j], sampled["ms_std"][j], \
+            sampled["m2s"][j], sampled["m2s_std"][j], sampled["m4s"][j], sampled["m4s_std"][j], \
+            sampled["m_sus"][j], sampled["m_sus_std"][j] = [float(x) for x in file.readline().strip().split(',')]
+            
+            sampled["T"][j] = 1.0 / sampled["beta"][j]
+            
+    return sim_info, sampled
