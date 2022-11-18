1300078434730086803a8c5fea24911e152c2291
João Inácio <j.g.c.inacio@fys.uio.no>
2022-11-18 12:21:36 +0100

    testing for run cluster files

--- run_job.sh
+++ run_job.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# Job name:
-#SBATCH --job-name=XY_L128
-#
-# Project:
-#SBATCH --account=ec12
-#
-# Wall time limit:
-#SBATCH --time=1-00:00:00
-#
-# Other parameters:
-#SBATCH --mem-per-cpu=3G
-#SBATCH --ntasks=1
+module --quiet purge  # Reset the modules to the system default
+module load GCC/11.2.0
+module load GCCcore/11.2.0
+module load Python/3.9.6-GCCcore-11.2.0
+module load SciPy-bundle/2021.10-foss-2021b
+module list
 
 PROGNAME=$0
 
@@ -28,20 +22,6 @@ Usage               : $PROGNAME [-c] [-h] [-n <n_threads>] [-t]
 EOF
     exit 1
 }
-
-## Set up job environment:
-set -o errexit  # Exit the script on any error
-set -o nounset  # Treat any unset variables as an error
-set -o pipefail
-
-module --quiet purge  # Reset the modules to the system default
-module load GCC/11.2.0
-module load GCCcore/11.2.0
-module load Python/3.9.6-GCCcore-11.2.0
-module load SciPy-bundle/2021.10-foss-2021b
-module list
-
-## Do some work:
 
 n_threads="4"
 test=""
@@ -75,5 +55,5 @@ make $cond $test
 
 echo "[4] - Running the simulation."
 echo
-./main $n_threads tmp/$vtx_name
+sbatch ./run_cluster.sh $n_threads $vtx_name
 echo "[4] - Finished the simulation."
