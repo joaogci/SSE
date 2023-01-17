23485d502856535acec2d84188180cd7a9766d57
João Inácio <j.g.c.inacio@fys.uio.no>
2023-01-17 11:14:06 +0100

    add arblib for cluster

--- submit_job.sh
+++ submit_job.sh
@@ -24,6 +24,7 @@ module load GCC/11.2.0
 module load GCCcore/11.2.0
 module load Python/3.9.6-GCCcore-11.2.0
 module load SciPy-bundle/2021.10-foss-2021b
+module load Arb/2.22.1-GCC-11.2.0
 module list
 
 ./main $1 $2
