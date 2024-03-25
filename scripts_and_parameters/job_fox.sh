80074ad7c008d220f23f0491bc23e06597ff9e16
João Inácio <j.g.c.inacio@fys.uio.no>
2024-03-25 16:47:15 +0100

    scripts to run multiple programs

--- scripts_and_parameters/job_fox.sh
+++ scripts_and_parameters/job_fox.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 # Job name:
-# no SBATCH --job-name=xy_L128
-#
+#SBATCH --job-name=Dir_R
 # Project:
 #SBATCH --account=ec12
-#
 # Wall time limit:
-# no SBATCH --time=1-00:00:00
-#
+#SBATCH --time=1-00:00:00
 # Other parameters:
 #SBATCH --ntasks=1
-# no SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=NCPU_R
 #SBATCH --mem-per-cpu=750M
 
 ## Set up job environment:
@@ -24,7 +21,7 @@ module load GCC/11.2.0
 module load GCCcore/11.2.0
 module load Python/3.9.6-GCCcore-11.2.0
 module load SciPy-bundle/2021.10-foss-2021b
-module load Arb/2.22.1-GCC-11.2.0
 module list
 
-./main $1 $2 $3
+$SSE_DIR/src/main NCPU_R
+
---
+++ scripts_and_parameters/run_sims.sh
@@ -0,0 +1,55 @@
+#!/bin/bash 
+
+Variable=(Run Lx_R Ly_R  BC_R S_R J_perp_R J_par_R H_R D_R Beta_R NT_R NSW_R NB_R NCPU_R)
+Name=( Run Lx_R Ly_R  BC_R S_R J_perp_R J_par_R H_R D_R Beta_R NT_R NSW_R NB_R NCPU_R )
+#      0    1    2     3    4      5      6      7   8     9    10   11   12    13
+{
+read -a Variable
+echo ${Variable[@]}
+echo ${Variable[0]}
+while [ ! ${Variable[0]} = "stop" ];   do
+    if [ ${Variable[0]} = "Y" ]; then
+        export B_R_dir=`echo ${Variable[9]} | sed s/"\.0"//`
+        
+        export Dir="Lx"${Variable[1]}"_Ly"${Variable[2]}"_Jperp"${Variable[4]}"_Jpar"${Variable[5]}"_h"${Variable[7]}"_D"${Variable[8]}"_beta"$B_R_dir
+        echo $Dir
+        if [ ! -e $Dir ]; then
+            mkdir $Dir
+            cd  $Dir
+            cp  ../Start/* . 
+            cd ..
+        else
+            cd $Dir
+            cp ../Start/parameters .
+            cp ../Start/job.sh .
+            cd ..
+        fi
+        cd $Dir
+        
+        let i=1
+        while [  $i -lt 13 ]; do
+            sed s/${Name[$i]}/${Variable[$i]}/    parameters  > tmp
+            mv tmp parameters
+            let i=i+1 
+        done
+        
+        sed s/Dir_R/$Dir/   job.sh  >  tmp
+        mv  tmp  job.sh
+        sed s/NCPU_R/${Variable[13]}/   job.sh  >  tmp
+        mv  tmp  job.sh
+  
+        job_id=$(sbatch job.sh | sed -e s/[^0-9]//g)
+        echo "run $job_id with name $Dir"
+        # for i in {1..1}; do
+        #   job_id=$(sbatch --dependency=afterok:$job_id job.sh | sed -e s/[^0-9]//g)
+        #   echo "run $job_id of dependency $i on $Dir"
+        # done
+
+        cd ..
+    fi
+    read -a Variable
+    echo ${Variable[@]}
+    echo ${Variable[0]}
+done
+}<sims
+
