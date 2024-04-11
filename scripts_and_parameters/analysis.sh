28b5ea86642913fcbbe22f9a2c04625100e7e94a
João Inácio <j.g.c.inacio@fys.uio.no>
2024-04-11 10:22:56 +0200

    scripts for running jobs

--- scripts_and_parameters/analysis.sh
+++ scripts_and_parameters/analysis.sh
@@ -1,17 +1,17 @@
 #!/bin/bash 
 
-Variable=(Run Lx_R Ly_R  BC_R S_R J_perp_R J_par_R H_R D_R Beta_R NT_R NSW_R NB_R NCPU_R)
-Name=( Run Lx_R Ly_R  BC_R S_R J_perp_R J_par_R H_R D_R Beta_R NT_R NSW_R NB_R NCPU_R )
-#      0    1    2     3    4      5      6      7   8     9    10   11   12    13
+Variable=(Run P_R Q_R  Nl_R S_R J_perp_R J_par_R Beta_R NT_R NSW_R NB_R NCPU_R)
+Name=( Run P_R Q_R  Nl_R S_R J_perp_R J_par_R Beta_R NT_R NSW_R NB_R NCPU_R )
+#      0    1    2    3   4     5      6       7      8     9    10   11  
 {
 read -a Variable
 echo ${Variable[@]}
 echo ${Variable[0]}
 while [ ! ${Variable[0]} = "stop" ];   do
     if [ ${Variable[0]} = "Y" ]; then
-        export B_R_dir=`echo ${Variable[9]} | sed s/"\.0"//`
+        export B_R_dir=`echo ${Variable[7]} | sed s/"\.0"//`
         
-        export Dir="Lx"${Variable[1]}"_Ly"${Variable[2]}"_Jperp"${Variable[4]}"_Jpar"${Variable[5]}"_h"${Variable[7]}"_D"${Variable[8]}"_beta"$B_R_dir
+        export Dir="p"${Variable[1]}"_q"${Variable[2]}"_nl"${Variable[3]}"_beta"$B_R_dir
         echo $Dir
         if [ ! -e $Dir ]; then
             mkdir $Dir
@@ -27,13 +27,12 @@ while [ ! ${Variable[0]} = "stop" ];   do
         cd $Dir
         
         let i=1
-        while [  $i -lt 13 ]; do
+        while [  $i -lt 12 ]; do
             sed s/${Name[$i]}/${Variable[$i]}/    parameters  > tmp
             mv tmp parameters
             let i=i+1 
         done
-
-        $SSE_DIR/src/ana.out *
+        $SSE_DIR/src/ana *
 
         cd ..
     fi
